b19852edadddf7bab605db01cf71d9c49da811c7
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-13 00:31:06 +0300

    Add project files.

---
+++ Lab12/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    static class Circle
+    {
+        static public double GetCircumference(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public double GetHypotenuse(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double perimeter = Circle.GetCircumference(r);  
+            double area = Circle.GetArea(r);
+            Console.WriteLine($"{perimeter} \n{area}");
+            Console.WriteLine("Введите координаты x,y");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double c = Circle.GetHypotenuse(x, y);
+            if (c > r)
+                Console.WriteLine("точка находится за пределами круга"); 
+            else
+                Console.WriteLine("точка принадлежит кругу");
+            Console.ReadKey();
+        }
+    }
+}
